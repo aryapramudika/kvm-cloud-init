7a89b97c5898ab4a27829b85fa3f9d20b7734fb3
Arya Pramudika <aryapramudika1@gmail.com>
2022-11-24 15:37:57 +0700

    Update script

--- kvm-provision.sh
+++ kvm-provision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-##Define your env, like a path,etc..
+##Define your env, like a dir,etc..
 
 
 VM_NAME="kworker1"
@@ -8,23 +8,26 @@ USERNAME="arya"
 PASSWORD="arya"
 CPU="1"
 MEMORY="1024"
-PATH="/AR"
+NETWORK="br-net"
+DIR="/AR"
 
 
-#mkdir -p $PATH/images/templates
-##Download cloud image ubuntu()
-#wget https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img -O $PATH/images/templates/ubuntu20server.qcow2
+##Jalankan sekali saja
+#mkdir -p $DIR/images/templates
+
+##Download cloud image ubuntu(Jalankan sekali saja)
+#wget https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img -O $DIR/images/templates/ubuntu20server.qcow2
 
 echo "[Convert & Copy Image] $VM_NAME"
-mkdir -p $PATH/images/$VM_NAME \
+mkdir -p $DIR/images/$VM_NAME \
 && qemu-img convert \
 -f qcow2 \
 -O qcow2 \
-$PATH/images/templates/ubuntu20-server.qcow2 \
-$PATH/images/$VM_NAME/root-disk.qcow2
+$DIR/images/templates/ubuntu20-server.qcow2 \
+$DIR/images/$VM_NAME/root-disk.qcow2
 
 echo "[Resize Disk...]"
-qemu-img resize $PATH/images/$VM_NAME/root-disk.qcow2 20G
+qemu-img resize $DIR/images/$VM_NAME/root-disk.qcow2 20G
 
 echo "[Create Cloud-Init Config...]"
 echo "#cloud-config
@@ -40,22 +43,22 @@ hostname: $VM_NAME
 # configure sshd to allow users logging in using password
 # rather than just keys
 ssh_pwauth: True
-" | sudo tee $PATH/images/$VM_NAME/cloud-init.cfg > /dev/null
+" | sudo tee $DIR/images/$VM_NAME/cloud-init.cfg > /dev/null
 
 echo "[Convert Cloud-Init Config...]"
 cloud-localds \
-$PATH/images/$VM_NAME/cloud-init.iso \
-$PATH/images/$VM_NAME/cloud-init.cfg
+$DIR/images/$VM_NAME/cloud-init.iso \
+$DIR/images/$VM_NAME/cloud-init.cfg
 
 echo "[VM $VM_NAME lagi dibuat..]"
 virt-install \
   --name $VM_NAME \
   --memory $MEMORY \
-  --disk $PATH/images/$VM_NAME/root-disk.qcow2,device=disk,bus=virtio \
-  --disk $PATH/images/$VM_NAME/cloud-init.iso,device=cdrom \
+  --disk $DIR/images/$VM_NAME/root-disk.qcow2,device=disk,bus=virtio \
+  --disk $DIR/images/$VM_NAME/cloud-init.iso,device=cdrom \
   --os-variant ubuntu20.04 \
   --virt-type kvm \
   --graphics none \
   --noautoconsole \
-  --network network=br-net\
+  --network network=$NETWORK\
   --import
